c085593da7b7744768de49cf53bb0959617942af
Felipe Llerena <llerenafelipe7@gmail.com>
2019-05-30 18:07:00 -0500

    Show News

--- src/components/New.js
+++ src/components/New.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
-const New = ({aNew}) => {
+const New = (aNew) => {
+  const { urlToImage, url, title, description, source } = aNew.new;
   return (
-    <div className="row">
-      
+    <div className="col s12 m6 l4">
+      <div className="card">
+        <div className="card-image">
+          <img src={urlToImage} alt={title} />
+        </div>
+        <div className="card-content">
+          <h3>{title}</h3>
+          <p>{description}</p>
+        </div>
+        <div className="card-action">
+          <a href={url} target="_blank" rel="noopener noreferrer" className="waves-effect waves-light btn">
+            Ver Noticia Completa
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
